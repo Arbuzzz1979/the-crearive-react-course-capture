0419f35cddad677af61cf1eb88e7d9cad9452081
Pavel Levschanov <levschanov@gmail.com>
2021-01-27 11:42:42 +0200

    7. Styling Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,39 +1,86 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
 
 const FaqSection = () => {
   return (
-<div className="faq">
-            <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start ?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, adipisci!</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, adipisci!</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, adipisci!</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
-            <div className="question">
-                <h4>What products du you offer.</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, adipisci!</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
+    <Faq>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start ?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            adipisci!
+          </p>
+          <p>Lorem, ipsum dolor.</p>
         </div>
-  )
-}
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            adipisci!
+          </p>
+          <p>Lorem, ipsum dolor.</p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            adipisci!
+          </p>
+          <p>Lorem, ipsum dolor.</p>
+        </div>
+        <div className="faq-line"></div>        
+      </div>
+      <div className="question">
+        <h4>What products du you offer.</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            adipisci!
+          </p>
+          <p>Lorem, ipsum dolor.</p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+    </Faq>
+  );
+};
 
-export default FaqSection
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0;
+    p {
+      padding: 1rem 0;
+    }
+  }
+`;
+
+export default FaqSection;
