78899d2d8b2fc9f91b9fad0fc2dfbda4e36beef7
Pavel Levschanov <levschanov@gmail.com>
2021-01-27 13:40:13 +0200

    9. React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
